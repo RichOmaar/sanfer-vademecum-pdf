6b242c47b3d257c5bafbdee4a81ce5ce90502938
RichOmaar <juanomcam@gmail.com>
2024-01-06 00:44:24 -0600

    Nuevas rutas apra probas

--- src/routes/index.js
+++ src/routes/index.js
@@ -53,4 +53,61 @@ router.post("/pdf", async (req, res) => {
   }
 });
 
+router.post("/pdf-blob", async (req, res) => {
+  const medicinsArray = req.body;
+  try {
+    const allData = await Promise.all(
+      medicinsArray.map((medicin) => fetchDataFromStrapi(medicin.id))
+    );
+    // res.setHeader("Content-Type", "application/pdf; charset=utf-8");
+    // res.setHeader("Content-Disposition", "attachment; filename=combined.pdf");
+
+    const pdfBuffers = [];
+
+    for (const data of allData) {
+      // Create a separate PDF for each medicine
+      const pdfBytes = await createPdf(data);
+      pdfBuffers.push(pdfBytes);
+    }
+
+    const combinedPdfBytes = await combinePdfs(pdfBuffers);
+
+    res.end('Blob');
+    res.end(Buffer.from(combinedPdfBytes));
+    
+  } catch (error) {
+    console.error("Error fetching data from Strapi:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+router.post("/pdf-64", async (req, res) => {
+  const medicinsArray = req.body;
+  try {
+    const allData = await Promise.all(
+      medicinsArray.map((medicin) => fetchDataFromStrapi(medicin.id))
+    );
+    // res.setHeader("Content-Type", "application/pdf; charset=utf-8");
+    // res.setHeader("Content-Disposition", "attachment; filename=combined.pdf");
+
+    const pdfBuffers = [];
+
+    for (const data of allData) {
+      // Create a separate PDF for each medicine
+      const pdfBytes = await createPdf(data);
+      pdfBuffers.push(pdfBytes);
+    }
+
+    const combinedPdfBytes = await combinePdfs(pdfBuffers);
+
+    console.log(combinedPdfBytes);
+    const base64String = Buffer.from(combinedPdfBytes).toString("base64");
+    res.end(base64String);
+    
+  } catch (error) {
+    console.error("Error fetching data from Strapi:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 export default router;
